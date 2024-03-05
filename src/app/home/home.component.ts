b8cca7179604e50d2cbb796708f352e31a50151a
James Schwartz <jschwartzdev@gmail.com>
2024-03-05 12:27:27 -0500

    pagination is fully functional

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnChanges } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { environment } from '../environment';
 import { Products, Product, Options } from '../../types';
@@ -12,14 +12,55 @@ import { ProductsComponent } from '../products/products.component';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   pageNum: number = 0;
+  products: any = [];
+  perPage: number = 10;
+  totalProductsInDB: number = 0;
+
+  requestOptions: any = {
+    params: { pageNum: this.pageNum, perPage: this.perPage },
+    responseType: 'json',
+  };
+
+  constructor(private productsService: ProductsService) {}
+
+  ngOnInit(): void {
+    this.fetchProducts();
+  }
+
+  fetchProducts() {
+    this.productsService
+      .getProducts(`${environment.apiUrl}/products`, this.requestOptions)
+      .subscribe((data: Products) => {
+        this.products = data.items;
+        this.totalProductsInDB = data.total;
+        this.addZero();
+        console.log(this.products, data);
+      });
+  }
+
+  addZero() {
+    this.products.forEach((product: Product) => {
+      if (product.price.length < 4) {
+        product.price += '0';
+      }
+    });
+  }
 
   nextPage() {
-    this.pageNum++;
+    if ((this.pageNum + 1) * this.perPage < this.totalProductsInDB) {
+      this.pageNum++;
+      this.requestOptions.params.pageNum++;
+      this.fetchProducts();
+    }
   }
 
   prevPage() {
-    this.pageNum--;
+    if (this.pageNum > 0) {
+      this.pageNum--;
+      this.requestOptions.params.pageNum--;
+      this.fetchProducts();
+    }
   }
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -11,37 +11,6 @@ import { environment } from '../environment';
   templateUrl: './products.component.html',
   styleUrl: './products.component.css',
 })
-export class ProductsComponent implements OnChanges {
-  @Input() pageNum?: number = 0;
-  constructor(private productsService: ProductsService) {}
-  products: any = [];
-  perPage: number = 10;
-
-  requestOptions: Options = {
-    params: { pageNum: this.pageNum, perPage: this.perPage },
-    responseType: 'json',
-  };
-
-  ngOnChanges(changes: SimpleChanges): void {
-    this.fetchProducts();
-    console.log(this.requestOptions);
-  }
-
-  fetchProducts() {
-    this.productsService
-      .getProducts(`${environment.apiUrl}/products`, this.requestOptions)
-      .subscribe((data: Products) => {
-        this.products = data;
-        this.addZero();
-        console.log(this.products);
-      });
-  }
-
-  addZero() {
-    this.products.forEach((product: Product) => {
-      if (product.price.length < 4) {
-        product.price += '0';
-      }
-    });
-  }
+export class ProductsComponent {
+  @Input() products: any = [];
 }
